053020d8895c3b1ae93140461988f9a26a83a78d
Hacker1337 <amirfvb@gamil.com>
2021-04-17 17:06:22 +0300

    New input format (from path file)

--- visual.py
+++ visual.py
@@ -2,17 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-dir = 'HeLa апоптоз'
 
-files = os.listdir(dir)
+pathfile = "visual.path"
+file = open(pathfile, encoding='utf-8')
+pathes = file.read().strip().split('\n')
 
-for f in files:
-    if f[-4:] == '.txt':
-        data = np.loadtxt(os.path.join(dir, f))
-        print(f, "opened successfully")
-        plt.pcolormesh(data)
-        plt.colorbar()
-        plt.savefig(os.path.join(dir, f[:-4] + '.png'), dpi=100)
-        # plt.show()
-        plt.close()
-        # plt.show()
+for dir in pathes:
+    files = os.listdir(dir)
+    print("\nNew path:", dir)
+
+    for f in files:
+        if f[-4:] == '.txt':
+            data = np.loadtxt(os.path.join(dir, f))
+            print(f, "opened successfully")
+            plt.pcolormesh(data)
+            plt.colorbar()
+            plt.savefig(os.path.join(dir, f[:-4] + '.png'), dpi=100)
+            # plt.show()
+            plt.close()
+            # plt.show()
